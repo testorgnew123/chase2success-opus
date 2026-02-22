4307ee0817a90a4aaf93d2411698b4e7972cab6e
honeyakshat999 <honeyakshat999@gmail.com>
2026-02-22 11:48:59 +0400

    fix issue of projects

--- src/lib/neon.ts
+++ src/lib/neon.ts
@@ -5,6 +5,7 @@ export const neon = createClient({
   auth: {
     url: import.meta.env.VITE_NEON_AUTH_URL,
     adapter: BetterAuthReactAdapter(),
+    allowAnonymous: true,
   },
   dataApi: {
     url: import.meta.env.VITE_NEON_DATA_API_URL,
